9aaeca5e675f64cb8479bae618a6d49047452ca0
pieralonso <pieralonso@proton.me>
2024-01-10 14:11:05 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-/*
-Al hacer click en un grado 
-1- cambiar el titulo
-2- generar las palabras en cada parte
-
-Al hacer click en los numeros de navegacion
-
-1 - pasar al siguiente nivel cambiar el nivel
-2 - mostrar la siguiente lista de palabras con sus partes
-*/
 import { duboisArray } from './duboisArray.js';
 
+// barra de navegacion 
