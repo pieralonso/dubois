262e70d7f2070f6b65edce906298bed2a9f674d9
pieralonso <pieralonso@proton.me>
2024-01-11 16:16:37 +0100

    updating javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 import { duboisArray } from './duboisArray.js';
 
+
 // barra de navegacion 
 document.addEventListener('DOMContentLoaded', function () {
 	var primaryButton = document.getElementById('primary-button');
 	var secondaryButton = document.getElementById('secondary-button');
 	var primaryMenu = document.getElementById('primary-menu');
 	var secondaryMenu = document.getElementById('secondary-menu');
+	const sectionTitle = document.getElementById('sub-section-title');
 	var navContainer = Array.from(document.getElementsByClassName('menu-container'));
+	var mainMenuLinks = Array.from(document.getElementsByClassName('main__menu-link'));
 	
 	primaryButton.addEventListener('click', function () {
 		primaryMenu.classList.toggle('hidden');
@@ -17,4 +20,12 @@ document.addEventListener('DOMContentLoaded', function () {
 		secondaryMenu.classList.toggle('hidden');
 		navContainer[0].classList.toggle('hidden');
 	});
+	
+	// generando secciones
+	
+	for (var element of mainMenuLinks) {
+		element.addEventListener('click', function (event) {
+			sectionTitle.textContent = event.target.textContent
+		})
+	}
 });
