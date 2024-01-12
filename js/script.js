860311da1100db3d592e75181ed45f6a2028ce66
pieralonso <pieralonso@proton.me>
2024-01-12 17:57:36 +0100

    js update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 import { duboisArray } from './duboisArray.js'
+
 // manejo del array 
-function echelons(a, b) {
-	var result = [];
-	for(var i = a; i <= b ; i++ ) {
-		result.push(`|${i}|`)
-	}
-	return result
-}
-function filtrarArrayConCriterio(duboisArray, array) {
-	return duboisArray.filter(item => array.some(element => item.includes(element)));
-  }
-  
+
+const dubois = duboisArray.slice().sort((a, b) => parseInt(a.split('|')[1]) - parseInt(b.split('|')[1]));
+
+const generarEchelon = (a, b) => Array.from({ length: b - a + 1 }, (_, i) => `|${a + i}|`);
+
+const filtrarPorEchelon = (echelons) => dubois.filter(item => echelons.some(e => item.includes(e)));
+
+const organizarNiveles = array => Array.from({ length: Math.ceil(array.length / 20) }, (_, i) => array.slice(i * 20, i * 20 + 20));
+
+const cpArray = filtrarPorEchelon(generarEchelon(1, 11));
+const ce2Array = filtrarPorEchelon(generarEchelon(12, 15));
+const cm1Array = filtrarPorEchelon(generarEchelon(16, 19));
+const cm2Array = filtrarPorEchelon(generarEchelon(20, 23));
+const s6EmeArray = filtrarPorEchelon(generarEchelon(24, 27));
+const c5EmeArray = filtrarPorEchelon(generarEchelon(28, 35));
+const t3EmeArray = filtrarPorEchelon(generarEchelon(36, 43));
+
+
+console.log(dubois.length)
+console.log(cpArray, ce2Array, cm1Array, cm2Array, s6EmeArray, c5EmeArray, t3EmeArray)
+//filtrarConCriterio
+// separaArray
+
 
 
 // barra de navegacion 
